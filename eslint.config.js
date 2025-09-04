971944726f8e86372d684c339c6cddf9e16ef488
Dhruv Gupta <druvgupta62@gmail.com>
2025-09-04 17:50:21 +0530

    Configured ESlint for the final time

---
+++ eslint.config.js
@@ -0,0 +1,15 @@
+const tseslint = require('typescript-eslint');
+
+module.exports = tseslint.config(
+    ...tseslint.configs.recommended,
+    {
+        rules: {
+            'semi': ['error', 'always'],
+            '@typescript-eslint/no-unused-vars': 'error',
+        },
+    },
+    {
+        // Add the config file itself to the ignore list
+        ignores: ['dist', 'node_modules', 'eslint.config.js'],
+    }
+);
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
-import express from "express";
 import serverConfig from "./config/serverConfig";
+import express from "express";
 
 
 const app = express();
 
 app.listen(serverConfig.PORT,()=>{
+   
     console.log(`Server started at ${serverConfig.PORT}`);
-})
+});
